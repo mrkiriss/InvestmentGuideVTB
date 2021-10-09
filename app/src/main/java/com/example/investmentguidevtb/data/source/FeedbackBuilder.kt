e87bb84902b5096655ce0b9edbf7bd6f49f2ed98
Kirill <kvoskrebentsev@mail.ru>
2021-10-09 23:45:49 +0300

    Practice and articals functions added

---
+++ app/src/main/java/com/example/investmentguidevtb/data/source/FeedbackBuilder.kt
@@ -0,0 +1,10 @@
+package com.example.investmentguidevtb.data.source
+
+import javax.inject.Inject
+
+class FeedbackBuilder @Inject constructor(){
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/example/investmentguidevtb/ui/practice/models/GameEndFeedback.kt
@@ -0,0 +1,9 @@
+package com.example.investmentguidevtb.ui.practice.models
+
+data class GameEndFeedback(
+    val goalResultPicture: String?,
+    val goalResultDescription: String?,
+    val riskResultPicture: String,
+    val riskResultDescription: String
+) {
+}
