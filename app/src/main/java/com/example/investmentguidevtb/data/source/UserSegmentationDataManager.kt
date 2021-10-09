659bdb68ac89f0d58e7195d7f832ceef3c7c1697
Arina <arina435@mail.ru>
2021-10-09 12:53:10 +0300

    Segmentation data manager created

---
+++ app/src/main/java/com/example/investmentguidevtb/data/source/UserSegmentationDataManager.kt
@@ -0,0 +1,21 @@
+package com.example.investmentguidevtb.data.source
+
+class UserSegmentationDataManager(
+) {
+    var risk: Float = -1f
+    get(){
+        return field
+    }
+    set(value){
+        field = -1f
+    }
+
+    var difficulty: Float = -1f
+        get(){
+            return field
+        }
+        set(value){
+            field = -1f
+        }
+
+}
